5a313178c5524fdd7f30a43db3318627c2638464
Maximilian Tiao <mtiao@vmware.com>
2023-01-04 13:20:50 -0800

    Finished initial word search algorithm, added timing code

--- w_s_solve.py
+++ w_s_solve.py
@@ -1,8 +1,9 @@
 import sys
 import getopt
 import subprocess
+import time
 
-def r_search(grid, word, index, x, y):
+def r_search(grid, word, index, x, y, dir):
     if (index == len(word)):
         return 1
     elif ((x < 0) or (y < 0) or (x == len(grid[0])) or (y == len(grid))):
@@ -10,22 +11,47 @@ def r_search(grid, word, index, x, y):
     else:
         if grid[y][x] == word[index]:
             index += 1
-            # Search N
-            return (r_search(grid, word, index, x, y - 1) | r_search(grid, word, index, x + 1, y - 1) \
-                |  r_search(grid, word, index, x + 1, y) | r_search(grid, word, index, x + 1, y + 1) \
-                |  r_search(grid, word, index, x, y + 1) | r_search(grid, word, index, x - 1, y + 1) \
-                |  r_search(grid, word, index, x - 1, y) | r_search(grid, word, index, x - 1, y - 1)) \
-                *  (index == len(word))
-        return 0
+            # Search N direction
+            if dir == "N" and r_search(grid, word, index, x, y - 1, "N") == 1:
+                return 1
+            # Search NE direction
+            elif dir == "NE" and r_search(grid, word, index, x + 1, y - 1, "NE") == 1:
+                return 1
+            # Search E direction
+            elif dir == "E" and r_search(grid, word, index, x + 1, y, "E") == 1:
+                return 1
+            # Search SE direction
+            elif dir == "SE" and r_search(grid, word, index, x + 1, y + 1, "SE") == 1:
+                return 1
+            # Search S direction
+            elif dir == "S" and r_search(grid, word, index, x, y + 1, "S") == 1:
+                return 1
+            # Search SW direction
+            elif dir == "SW" and r_search(grid, word, index, x - 1, y + 1, "SW") == 1:
+                return 1
+            # Search W direction
+            elif dir == "W" and r_search(grid, word, index, x - 1, y, "W") == 1:
+                return 1
+            # Search NW direction
+            elif dir == "NW" and r_search(grid, word, index, x - 1, y - 1, "NW") == 1:
+                return 1
+            else:
+                return 0
+        else:
+            return 0
 
 def base_search(grid, word):
     # Iterate through each row of grid
     for y in range(len(grid)):
         for x in range(len(grid[y])):
             # Find first letter
-            if grid[y][x] == word[0].capitalize():
-                r_search(grid, word, 0, x, y)
-                print("%s found at (%d, %d)" % (word, x, y))
+            if grid[y][x] == word[0]:
+                status_code = r_search(grid, word, 0, x, y, "N") + r_search(grid, word, 0, x, y, "NE") \
+                            + r_search(grid, word, 0, x, y, "E") + r_search(grid, word, 0, x, y, "SE") \
+                            + r_search(grid, word, 0, x, y, "S") + r_search(grid, word, 0, x, y, "SW") \
+                            + r_search(grid, word, 0, x, y, "W") + r_search(grid, word, 0, x, y, "NW")
+                if status_code == 1:
+                    print("%s found at (%d, %d)" % (word, x, y))
 
 
 if __name__ == "__main__":
@@ -60,20 +86,29 @@ if __name__ == "__main__":
             print("ERROR: Words need to be listed", file=sys.stderr)
         sys.exit(-1)
     '''
-
-    words = "dog cat pig"
-    word_search_command = "word-search" + " " + words
+    '''
+    Word Search #146
+    '''
+    words = "action adrift airplane approach blacktop bridesmaid burrow cardinal caviar coffee dangle engineer eyetooth \
+            foothold heliport herdsman lawman needless pinwheel pliers porter praise recent relation resistance retain  \
+            saucer senior serenade streamline toddle treaty triangle underway whiten"
+    word_search_command = "word-search -s 50 -d 7 " + words
     word_search_process = subprocess.Popen(word_search_command.split(), stdout=subprocess.PIPE)
     output, error = word_search_process.communicate()
     raw_puzzle = output.decode().split('\n')[3:-6]
     puzzle_grid = []
     for row in raw_puzzle:
         puzzle_grid.append(row.split(" "))
-        print(puzzle_grid[-1])
 
+    for y in puzzle_grid:
+        for x in y:
+            print(x + " ", end="")
+        print("")
+    start = time.perf_counter()
     for word in words.split():
-        print("Finding %s" % (word))
-        base_search(puzzle_grid, word)
+        base_search(puzzle_grid, word.upper())
+    end = time.perf_counter()
+    print(f"Solved word search in {end - start:0.4f} seconds")
 
     
 
