3287388ee91674fc9d78c9b7616b26670fd7408a
Maximilian Tiao <maximilian.tiao@gmail.com>
2023-01-14 13:30:08 -0800

    Added setup files

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
 from setuptools import setup, find_packages
-import pathlib
-
-here = pathlib.Path(__file__).parent.resolve()
-
-long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="word-search-solver",
     version="1.0.0",
+    license='MIT',
     description="The amazing Word Search Solver",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/maximiliantiao/word_search_solver",
     author="Maximilian Tiao",
     author_email="maximilian.tiao@gmail.com",
@@ -23,15 +17,6 @@ setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     python_requires=">=3.7",
-    extras_require={  # Optional
-        "dev": ["check-manifest"],
-        "test": ["coverage"],
-    },
-    entry_points={  # Optional
-        "console_scripts": [
-            "sample=sample:main",
-        ],
-    },
     project_urls={  # Optional
         "Bug Tracking": "https://github.com/maximiliantiao/word_search_solver/issues",
         "Source": "https://github.com/maximiliantiao/word_search_solver"
